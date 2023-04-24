9afb2bbc18e2dec9a11c614d7f1a1fbdbf104780
El Mehdi Naciri <90041668+maj3stiic@users.noreply.github.com>
2023-04-25 02:13:34 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <title>PHP Test</title>
 </head>
 <body>
-<?php echo '<p>Hello World</p>'; ?>
+<?php echo '<p>Hello World Mehdi</p>'; ?>
   echo "<img src='https://miro.medium.com/v2/resize:fit:4800/format:webp/0*oyD7ekV-hMU91h4J.png'/>";
 </body>
 </html>
